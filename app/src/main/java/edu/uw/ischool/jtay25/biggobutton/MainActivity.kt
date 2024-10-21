15e6dcd6a1956133d8db3c83a650a276e98e77ab
Joshua Taylor <joshuaetaylor231@gmail.com>
2024-10-21 15:16:45 -0700

    quick fix

--- app/src/main/java/edu/uw/ischool/jtay25/biggobutton/MainActivity.kt
+++ app/src/main/java/edu/uw/ischool/jtay25/biggobutton/MainActivity.kt
@@ -3,7 +3,6 @@ package edu.uw.ischool.jtay25.biggobutton
 import android.graphics.Color
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
